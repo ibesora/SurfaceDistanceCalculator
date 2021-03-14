ffc2fa8e5d9fa2083f76c215c06485301a8deb7b
ibesora <isaac.besora@gmail.com>
2021-03-14 20:23:43 +0100

    Non axis-aligned path

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
-from utils import line, getMeters, surfaceDistance
+from utils import line, getMeters, surfaceDistance, getQuadrant, Quadrants, diagonal, top, bottom, left, right, Hit, CellEdges, interpolateHeight
 
 def computeSurfaceDistance(start, end, data, metersPerPixel, metersPerHeightValue):
   path = line(start, end)
   if (path.length == 0): return [{"accumSurfaceDistance": 0}]
   if (path.getStart()[1] == path.getEnd()[1]): return rowDistance(path, data, metersPerPixel, metersPerHeightValue)
   if (path.getStart()[0] == path.getEnd()[0]): return columnDistance(path, data, metersPerPixel, metersPerHeightValue)
-  return []
+  return nonAxisAlignedDistance(path, data, metersPerPixel, metersPerHeightValue)
 
 def rowDistance(line, data, metersPerPixel, metersPerHeightValue):
   return axisAlignedDistance(line, data, 0, metersPerPixel, metersPerHeightValue)
@@ -27,11 +27,11 @@ def axisAlignedDistance(line, data, index, metersPerPixel, metersPerHeightValue)
     prevAccumSD = prevStep["accumSurfaceDistance"]
     stepDist = metersPerPixel
     if (index == 0):
-        # Traversing columns
-        stepHeight = getMeters(int(start[0]) + i, int(start[1]), data, metersPerHeightValue)
+      # Traversing columns
+      stepHeight = getMeters(int(start[0]) + i, int(start[1]), data, metersPerHeightValue)
     else:
-        # Traversing rows
-        stepHeight = getMeters(int(start[0]), int(start[1]) + i, data, metersPerHeightValue)
+      # Traversing rows
+      stepHeight = getMeters(int(start[0]), int(start[1]) + i, data, metersPerHeightValue)
     stepAccumDist = prevAccum + metersPerPixel
     stepSurfaceDist = surfaceDistance(stepDist, stepHeight - prevHeight)
     stepsInfo.append(buildStepData(stepDist, stepHeight, stepAccumDist, stepSurfaceDist, prevAccumSD + stepSurfaceDist))
@@ -45,3 +45,71 @@ def buildStepData(dist, height, accumDistance, surfaceDist, accumSurfaceDistance
     "surfaceDistance": surfaceDist,
     "accumSurfaceDistance": accumSurfaceDistance
   }
+
+def nonAxisAlignedDistance(path, data, metersPerPixel, metersPerHeightValue):
+  quadrant = getQuadrant(path)
+  start = path.getStart()
+  end = path.getEnd()
+  currentCell = [start[0], start[1]]
+  stepsInfo = []
+  lastIntersectingPoint = start
+      
+  stepsInfo.append(buildStepData(0, getMeters(int(start[0]), int(start[1]), data, metersPerHeightValue), 0, 0, 0))
+  while currentCell[0] != end[0] or currentCell[1] != end[1]:
+    (intersectionHits, cellIndexUpdater) = computeIntersections(path, getLinesToTest(quadrant, currentCell))
+    
+    for hit in intersectionHits:
+      prevStep = stepsInfo[len(stepsInfo) - 1]
+      prevAccum = prevStep["accumDistance"]
+      prevHeight = prevStep["height"]
+      prevAccumSD = prevStep["accumSurfaceDistance"]
+      stepDist = line(lastIntersectingPoint, (hit.getGeom().x, hit.getGeom().y)).length() * metersPerPixel
+      startVertex = hit.getLine().getVertices()[0]
+      endVertex = hit.getLine().getVertices()[1]
+      startVertexHeight = getMeters(int(startVertex[0]), int(startVertex[1]), data, metersPerHeightValue)
+      endVertexHeight = getMeters(int(endVertex[0]), int(endVertex[1]), data, metersPerHeightValue)
+      stepHeight = interpolateHeight(startVertexHeight, endVertexHeight, hit.getNormalizedDistanceOnLine())
+      stepAccumDist = prevAccum + stepDist
+      stepSurfaceDist = surfaceDistance(stepDist, stepHeight - prevHeight)
+      stepsInfo.append(buildStepData(stepDist, stepHeight, stepAccumDist, stepSurfaceDist, prevAccumSD + stepSurfaceDist))
+      lastIntersectingPoint = (hit.getGeom().x, hit.getGeom().y)
+    
+    currentCell[0] += cellIndexUpdater[0]
+    currentCell[1] += cellIndexUpdater[1]
+  return stepsInfo
+
+def computeIntersections(path, lines):
+  def distanceSorter(hit):
+    return line(path.getStart(), hit.getGeom()).length()
+  
+  intersections = {}
+  cellUpdater = [0, 0]
+  
+  for i in range(0, len(lines)):
+    intersection = path.intersection(lines[i])
+    if (intersection.geom_type == 'Point'):
+      intersections[intersection.wkt] = Hit(intersection, lines[i])
+      cellUpdater = updateCellIndexUpdater(lines[i].getName(), cellUpdater)
+  uniqueIntersections = list(intersections.values())
+  uniqueIntersections.sort(key=distanceSorter)
+  return (uniqueIntersections, cellUpdater)
+
+def getLinesToTest(quadrant, currentCell):
+  lines = []
+  if (quadrant == Quadrants.First):
+    lines = [diagonal(currentCell), top(currentCell), right(currentCell)]
+  elif (quadrant == Quadrants.Second): 
+    lines = [diagonal(currentCell), top(currentCell), left(currentCell)]
+  elif (quadrant == Quadrants.Third): 
+    lines = [diagonal(currentCell), bottom(currentCell), left(currentCell)]
+  elif (quadrant == Quadrants.Fourth): 
+    lines = [diagonal(currentCell), bottom(currentCell), right(currentCell)]
+  return lines
+
+def updateCellIndexUpdater(name, cellUpdater):
+  newCellIndexUpdater = cellUpdater
+  if (name == CellEdges.Top): return [cellUpdater[0], cellUpdater[1] - 1]
+  elif (name == CellEdges.Bottom): return [cellUpdater[0], cellUpdater[1] + 1]
+  elif (name == CellEdges.Left): return [cellUpdater[0] - 1, cellUpdater[1]]
+  elif (name == CellEdges.Right): return [cellUpdater[0] + 1, cellUpdater[1]]
+  return cellUpdater
--- utils.py
+++ utils.py
@@ -1,24 +1,74 @@
 import math
+from enum import Enum
 from shapely.geometry import LineString
 
-class Line:
-  def __init__(self, start, end):
-      self.geom = LineString([start, end])
-  def __str__(self):
-      return str((str(self.geom)))
-  def getGeom(self):
-      return self.geom
-  def getStart(self):
-      return self.geom.coords[0]
-  def getEnd(self):
-      return self.geom.coords[1]
-  def length(self):
-      return self.geom.length
-  def intersection(self, lineToIntersect):
-      return self.geom.intersection(lineToIntersect.geom)
+Quadrants = Enum('Quadrants', 'First Second Third Fourth')
+CellEdges = Enum('CellEdges', 'Top Bottom Left Right Diagonal')
 
-def line(a, b):
-    return Line(a, b)
+class Line:
+  def __init__(self, start, end, name):
+    self.vertices = [start, end]
+    self.geom = LineString([start, end])
+    self.name = name
+  def __str__(self):
+    return str((self.name, str(self.geom)))
+  def getVertices(self):
+    return self.vertices
+  def getGeom(self):
+    return self.geom
+  def getName(self):
+    return self.name
+  def getStart(self):
+    return self.geom.coords[0]
+  def getEnd(self):
+    return self.geom.coords[1]
+  def length(self):
+    return self.geom.length
+  def intersection(self, lineToIntersect):
+    return self.geom.intersection(lineToIntersect.geom)
+
+class Hit:
+  def __init__(self, point, line):
+    self.geom = point
+    self.line = line
+  def __str__(self):
+    return str((str(self.geom), str(self.line)))
+  def getGeom(self):
+    return self.geom
+  def getLine(self):
+    return self.line
+  def getNormalizedDistanceOnLine(self):
+    return Line(self.line.getStart(), self.geom, "Hit edge").length() / self.line.length()
+
+def line(a, b, name = "Custom"):
+  return Line(a, b, name)
+
+def translatePoint(p, v):
+  return (p[0] + v[0], p[1] + v[1])
+
+def diagonalLine(start, name):
+  return line(start, translatePoint(start, (-1, 1)), name)
+
+def verticalLine(start, name):
+  return line(start, translatePoint(start, (0, 1)), name)
+
+def horizontalLine(start, name):
+  return line(start, translatePoint(start, (1, 0)), name)
+
+def top(cellIndex):
+  return horizontalLine(cellIndex, CellEdges.Top)
+
+def bottom(cellIndex):
+  return horizontalLine(translatePoint(cellIndex, (0, 1)), CellEdges.Bottom)
+
+def left(cellIndex):
+  return verticalLine(cellIndex, CellEdges.Left)
+
+def right(cellIndex):
+  return verticalLine(translatePoint(cellIndex, (1, 0)), CellEdges.Right)
+
+def diagonal(cellIndex):
+  return diagonalLine(translatePoint(cellIndex, (1, 0)), CellEdges.Diagonal)
 
 def getTextureData(x, y, data):
   return data[y][x]
@@ -28,3 +78,19 @@ def getMeters(x, y, data, metersPerHeightValue):
 
 def surfaceDistance(xDist, yDist):
   return math.sqrt(xDist*xDist + yDist*yDist)
+
+def getQuadrant(line):
+  start = line.getStart()
+  startCol = start[0]
+  startRow = start[1]
+  end = line.getEnd()
+  endCol = end[0]
+  endRow = end[1]
+  if (startCol < endCol and startRow >= endRow): return Quadrants.First
+  if (startCol >= endCol and startRow > endRow): return Quadrants.Second
+  if (startCol > endCol and startRow <= endRow): return Quadrants.Third
+  if (startCol <= endCol and startRow < endRow): return Quadrants.Fourth    
+
+def interpolateHeight(startHeight, endHeight, k):
+  height = startHeight + (endHeight - startHeight)*k
+  return height
